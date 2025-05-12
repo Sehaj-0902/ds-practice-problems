a486bfabe62bb8d5a701adda2b5622a94c16eb47
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-05-12 18:10:17 +0530

    [Sehaj] ADD: Left rotation array problem

---
+++ src/LeftRotation.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result1 {
+
+    /*
+     * Complete the 'rotateLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+        // Write your code here
+        List <Integer> rotatedList = new ArrayList<>();
+        for (int i = d; i < arr.size() ; i++) {
+            rotatedList.add(arr.get(i));
+        }
+        for (int i = 0; i < d; i++) {
+            rotatedList.add(arr.get(i));
+        }
+        return rotatedList;
+    }
+}
+
+public class LeftRotation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result1.rotateLeft(d, arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
